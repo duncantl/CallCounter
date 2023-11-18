61f38f1bfcbcdceab564044e010e228f0bb5034d
Duncan Temple Lang <duncan@r-project.org>
2023-11-18 14:41:36 -0800

    new function timer helpers

--- R/timeFun.R
+++ R/timeFun.R
@@ -3,26 +3,106 @@ function()
 {
     startTime = NULL
     times = numeric()
-    start = function() {
-    #    message("start")
+    start = function() 
         startTime <<- Sys.time()
-    }
-    end = function() {
-    #    message("end")
-        times[[ length(times) + 1L ]] <<- Sys.time() - startTime
-    }
 
-    list(start = start, end = end, times = function() times, reset = function() times <<- numeric())
+    end = function() 
+        times[[ length(times) + 1L ]] <<- Sys.time() - startTime
+
+    invisible(list(start = start, end = end, times = function() times, reset = function() times <<- numeric()))
 }
 
 
 timeFun =
-function(fun, tmr = genFunTimer(), where = globalenv(), ...)
+function(fun, tmr = genFunTimer(), where = globalenv(), print = FALSE, character.only = FALSE, ...)
 {
-    funName = as.character(substitute(fun))
-    trace(funName, tmr$start, exit = tmr$end, ..., where = where)
+    funName = if(character.only)
+                fun
+              else
+                as.character(substitute(fun))
+    trace(funName, tmr$start, exit = tmr$end, print = print, ..., where = where)
     tmr 
 }
+
+timeFunMulti =
+function(fun, tmr = genFunTimer(), where = globalenv(), print = FALSE, ncalls = NULL, ...)
+{
+    if(length(ncalls))
+        ncalls = structure(rep(0L, length(fun)), names = fun)
+    timers = mapply(timeFun, fun, ncalls, MoreArgs = list(where = where, print = print, ..., character.only = TRUE))    
+#    timers = lapply(fun, timeFun, where = where, print = print, ..., character.only = TRUE)
+    names(timers) = fun
+    function()
+        lapply(timers, function(x) x$times())
+}
+
+
+
+######################################
+
+genMultiFunTimer =
+function(funNames, ncalls = NULL)
+{
+    nfuns = length(funNames)
+
+    startTime = NULL
+
+     # numeric vector won't work. Need the class POSIXct
+    reset = function()
+        startTime <<- structure(vector("list", nfuns), names = funNames)
+    
+    reset()
+    
+    times = structure(vector("list", nfuns), names = funNames)
+    
+    start = function(fun) {
+        startTime[[fun]] <<- Sys.time()
+    }
+    
+    end = function(fun) {
+        times[[fun]] [ length(times[[fun]]) + 1L ] <<- Sys.time() - startTime[[fun]]
+    }
+
+    list(start = start, end = end, times = function() times, reset = reset)
+}
+
+
+timeFunMulti2 =
+function(funNames, tmr = genMultiFunTimer(funNames, ncalls), where = globalenv(), print = FALSE,
+         ncalls = NULL,   ...)
+{
+    for(i in funNames) {
+
+       if(FALSE) {
+        start = substitute(fun(funName), list(fun = tmr$start, funName = i))
+        end = substitute(fun(funName), list(fun = tmr$end, funName = i))
+   } else {
+#       force(start); force(end)
+
+
+           start = quote(f(name))
+           start[c(1, 2)] = list(tmr$start, i)
+           end = quote(f(name))
+           end[c(1, 2)] = list(tmr$end, i)           
+       }
+#        browser()
+        trace(i, start, exit = end, print = print, ..., where = where)
+    }
+    
+    tmr     
+}
+
+#lapply(funNames, function(i) {
+#          start = substitute(fun(funName), list(fun = tmr$start, funName = i))
+#          end = substitute(fun(funName), list(fun = tmr$end, funName = i))
+#          trace(i, start, exit = end, print = print, ..., where = where)
+#})
+
+##############################
+
+
+
+
 
 if(FALSE) {
 
